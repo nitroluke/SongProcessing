485a4c1fc8cc34fb4995f73fdcdd9b1c901ad5f7
nitroluke <nitro.luke@gmail.com>
2015-02-18 03:34:21 -0700

    working repeating song names, need to implement stop Words next

--- SongProcessing.pl
+++ SongProcessing.pl
@@ -18,7 +18,7 @@
 #print "hello $b"
 #}
 # Replace the string value of the following variable with your names.
-use Data::Dumper qw(Dumper);
+use Data::Dumper qw(Dumper);    #print Dumper %hash
 my $name = "<Luke Welna>";
 my $partner = "<?>";
 print "CSCI 305 Lab 1 submitted by $name and $partner.\n\n";
@@ -40,122 +40,90 @@ my $input;
 my $songCount = 0;
 # This loops through each line of the file
 while($line = <INFILE>){
+    $line =~ s{\s}{ }g;
+   chomp($line);
    @token = split('<SEP>',$line); # a token array holding each part of the delimited string
    $song = $token[3];                    # sets the song title to the last part of the token array
    $song =~ s/\(.+|\[.+|\{.+|\\.+|\/.+|\_.+|\-.+|\:.+|\".+|\`.+|\+.+|\=.+|\*.+//g;
    #$song =~ s/[\\\/\`\(\{\[\*\+\-_=:].*//;     #add ( remove everything after one of the following chars \, /, -, :, (, {, [, _, `,+,=
    $song =~ s/feat. .*//;                           # removes everything after "feat."
-   $song =~ s/[\?\¿\!\¡\.\;\%\@\#\|\&\$]*//g;  #removes puncuation from the song titles
-   $song =~ s{\s}{ }g;
+   $song =~ s/[\?\¿\!\¡\.\;\%\@\#\|\&\$]//g;  #removes puncuation from the song titles
+
    if($song !~ /([^(\w|\s|\')])/){                  #checks for english words
 
       $song = lc($song);                         # sets all songs to lowercase
-      #$songCount++;                               # incriment song counter
-#     push(@cleanSongs, $song);        #pushes all of the songs on the array (just using this for counting)
-     chomp;
+      $songCount++;                               # incriment song counter
+     #chomp;
      my @words = split(/ /, $song);       # splits the song on spaces and places it in an array called words
      my $numWords = @words;           # sets the number of words in the song title to a variable called $numWords
      #my $frequency = 0;                         # reset frequency
      for (my $i = 0; $i < $numWords - 1; $i++){       # iterates through each song title, word by word
          my $temp = $words[$i];
          my $temp2 = $words[$i+1];
+         if(length($temp) == 0 || length($temp2) == 0 ){
+         }else{
          $hash{$temp}{$temp2}++;                    #increment the frequency for this hashSet by 1;
-         print "$temp ";
-      #   if(exists $hash{$words[$i]}{$words[$i+1]}){  #if this combination exists already, update the frequency... something ain't right
-      #     print "hash{$words[$i]]}{$temp} Exists\n";
-      #     #retrieve the frequency given a word and reset it
-      #     #$frequency = $hash{$words[$i]}{$words[$i+1]}; ## not quite right either?? set current frequency to the frequency for this specific hash
-      #     $hash{$words[$i]}{$words[$i+1]}++;                    #incriment frequency by 1;
-      #   }else{  #otherwise add a new value to the hash.
-      #     $hash{$words[$i]}{$words[$i+1]} = $frequency++;
-      #     print "adding hash{$words[$i]}{$temp} = $frequency\n";
-      # }
-     #print "\n";
+#         print "$temp ";
+       }
     }
-    print "\n";
+    #print "\n";
   }
 }
 # Close the file handle
 close INFILE;
-
+my $runs = 0;
+my $nextStr == "";
 # At this point (hopefully) you will have finished processing the song
 # title file and have populated your data structure of bigram counts.
 print "\nFile parsed. Bigram model built.\n\n";
 print "The number of cleaned up songs = $songCount\n";
 # User control loop
-while ($input ne "q"){
+do{
   # Replace these lines with some useful code
-  print "Enter a word [Enter 'q' to quit]: ";
-  $input = <STDIN>;
-  chomp($input);
-  my %values = %{$hash{$input}};
-  my $numValues = @values;
-  my %keys = keys %{$hash{$input}};
-  if($input eq "q"){
-    exit 0;
+  if($runs == 0){  # if this is the first run of the do while, ask input from the user
+    print "Enter a word [Enter 'q' to quit]: ";
+    $input = <STDIN>;
+    chomp($input);
+    if($input eq "q"){
+      print("I DECLARE BANKRUPTCY\n");
+      exit 0;
+    }
+    print("$input ");
   }
-  foreach $input(keys %values){
-    my $count = $values{$input};
-    print "$input $count \n";
+  $runs++;
+  if($runs <= 20){
+    $input = mcw($input);
+     print("$input ");
+  }else{
+    print("\n");
+    $runs = 0;
   }
-
-  # for (my $i = 0; $i< $numValues; $i++){
-  #   print("$input -> $values[$i] -> $keys[$i]");
-  # }
-  #print($hash{$input});
-
-  #print Dumper %hash;
-
-  # print "\n";
-  # print "Value Exists, but may be undefined \n"  if exists      $hash{$input}; ## something isnt working correctly
-  # print "Value is Defined, but may be false \n"    if defined  $hash{$input};     ## always prints
-  # print "Value is True at hash key $input \n\n"        if                $hash{$input};
-
-#Outline for what i will need to do
-# if (exists $hash{$input}){
-#   my $wordFrequency = getFrequency($input);
-#   my $followingWord = ; #retrieve information out of the hash to find the word that follows another
-#   print"The most common word to follow $input is $followingWord\n";
-#   print"The word $followingWord follows $input $wordFrequency times.\n";
-# }else{  # if the word is not in the in any song title
-#   print"That word is not in a song title";
-# }
-}
+}while ($input ne "q");
 
 # MORE OF YOUR CODE HERE....
-# sub getFrequency{   #unnecessary subroutine
-#   my $arg1 = shift @_;
-#   $frequency = shift @_;
-#   if(Arg1 eq ' '){
-#     $frequency = 0;
-#   }
-
-#   my $uniqueNewyork = (split(/ /, Arg1))[-1];
-#   print "\n$uniqueNewyork\n";
-#   return $uniqueNewyork;
-# }
-
-
-
-# sub processString{
-#     my ($song) = @_;
-#     # Removing everything after these characters from the song title
-#     $song =~ s/[\\\/\-\:({[_-`+=].*//;     #remove everything after one of the following chars \, /, -, :, (, {, [, _, `,+,=
-#     $song =~ s/feat. .*//;                       #deleting everything after"feat." from the string
-#     $song =~ tr/[?¿!¡.;%@#|&$]//d;    #deleting punctuation from song title
-
-# #     if($song =~/.*'.*/){  # keeps everything with apostraphes in the song array
-# #         push (@cleanSongs, $song);
-# # #        print "$song\n";
-# #     }
-# #     if($song =~ /^[[:alpha:]]+$/){ #removes everything with a non-english char.
-# #         push (@cleanSongs, $song);
-# #   #      print "$song\n";
-# #     }
-
-
-#     #With non-english characters
-#     push (@cleanSongs, $song);
-#     print "$song";
-# }
+sub mcw{
+  my %values = %{$hash{$input}};          #%values is a hash which contains the word that follows input and its frequency
+  #  my %keys = keys %{$hash{$input}};
+  my $distinct = keys %{$hash{$input}};   # this is the number of words that follow $input
+  my $max = 0;
+  my $maxStr;
+  foreach $key (keys %values){                #key is the string that follows a word
+    my $count = $values{$key};                  #count is the number of values(frequency) per for a key(string);
+    if($max < $count){
+      $max = $count;
+      $maxStr = $key;
+    }
+  #     print("__________________\n");
+  #     print "$key -> $count \n";
+  #     print("__________________\n");
+  }
+    # print("$maxStr occurs $max times\n");
+    # print("The most common word to occur after $input = $maxStr\n");
+    # print("There are $distinct distinct words that follow the word $input\n");
+  if($distinct == 0){
+    return undef;
+  }else{
+    return $maxStr;
+  }
+}
 
